b7ae1c169ec8ca1a62ea982746989cb664d8834d
rayolben <luismanuel.rosario.sss6a@gmail.com>
2025-11-14 17:09:40 -0500

    agregar navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faKeyboard, faUserCircle, faCarTunnel, faSquareRootVariable, faDatabase } from "@fortawesome/free-solid-svg-icons";
 
-export const NavBar = () => {
+export const NavBar = ({ setSelected }) => {
+  const options = [
+    {
+      id: 1,
+      name: "Tablero",
+      icon: <FontAwesomeIcon icon={faKeyboard} />,
+    },
+    {
+      id: 2,
+      name: "Clientes",
+      icon: <FontAwesomeIcon icon={faUserCircle} />,
+    },
+    {
+      id: 3,
+      name: "Vehiculos",
+      icon: <FontAwesomeIcon icon={faCarTunnel} />,
+    },
+    {
+      id: 4,
+      name: "Servicios",
+      icon: <FontAwesomeIcon icon={faSquareRootVariable} />,
+    },
+    {
+      id: 5,
+      name: "Ordenes",
+      icon: <FontAwesomeIcon icon={faDatabase} />,
+    },
+  ];
 
-    const options = [
-        
-    ]
-
-    return (
-        <div className="border rounded w-40">
-
-
-
+  return (
+    <div className="border rounded w-40 p-4 flex flex-col gap-3">
+      {options.map((item) => (
+        <div onClick={() => setSelected(item.name)} className="flex gap-2 items-center cursor-pointer" id={item.id}>
+          {item.icon}
+          <p>{item.name}</p>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import { NavBar } from "../components/NavBar";
 
+const componentes = {
+  Tablero: <div>Tablero</div>,
+  Clientes: <div>Clientes</div>,
+  Vehiculos: <div>Vehiculos</div>,
+  Servicios: <div>Servicios</div>,
+  Ordenes: <div>Ordenes</div>,
+};
+
 export const AdminPage = () => {
+  const [selected, setSelected] = useState("Tablero");
+
   return (
-    <div className="flex">
-      <NavBar />
+    <div className="flex gap-2 p-8">
+      <NavBar selected={selected} setSelected={setSelected} />
       <div>
         <h1>Bienvenido Administrador</h1>
+        <div>{componentes[selected]}</div>
       </div>
     </div>
   );
